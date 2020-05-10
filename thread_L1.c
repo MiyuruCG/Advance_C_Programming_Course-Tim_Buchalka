c8d309cb5c9daadd9d167afbc88350e81ba101a5
MiyuruCG <miyurugunatilake@gmail.com>
2020-05-10 09:27:46 +0530

    thread Lec 1

---
+++ thread_L1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *hello_func()
+{
+    printf("Hello world.. \n");
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t thread = 0;
+    pthread_create(&thread, NULL, hello_func, NULL);
+    pthread_join(thread, NULL);
+    pthread_exit(NULL);
+
+    return 0;
+}
